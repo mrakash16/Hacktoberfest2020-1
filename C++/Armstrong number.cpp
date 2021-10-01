3b6a5c432f277f9d8e04d1b82a918d8c97f5fc47
vikasbaghel1001 <75917410+vikasbaghel1001@users.noreply.github.com>
2021-10-01 09:37:06 +0530

    Update Armstrong number.cpp

--- C++/Armstrong number.cpp
+++ C++/Armstrong number.cpp
@@ -4,19 +4,19 @@
 using namespace std;
 int main()
 {
-int n,r,sum=0,temp;
-cout<<"Enter the Number=  ";
-cin>>n;
-temp=n;
-while(n>0)
-{
-r=n%10;
-sum=sum+(r*r*r);
-n=n/10;
-}
-if(temp==sum)
-cout<<"The number given is an Armstrong Number."<<"\n";
-else
-cout<<"The number given is NOT Armstrong Number."<<"\n";
-return 0;
+   int n,r,sum=0,temp;
+    cout<<"Enter the Number=  ";
+    cin>>n;
+     temp=n;
+     while(n>0)
+     {
+        r=n%10;
+        sum=sum+(r*r*r);
+         n=n/10;
+      }
+   if(temp==sum)
+   cout<<"The number given is an Armstrong Number."<<"\n";
+   else
+   cout<<"The number given is NOT Armstrong Number."<<"\n";
+   return 0;
 }
